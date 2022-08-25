18b244eb4811f7872f367966510968c62d6f42ea
River <riverliway@gmail.com>
2022-08-25 09:39:57 +0200

    fix(#24): mock octogration to fail outside of a github action

--- src/API/octokitWrapper.test.ts
+++ src/API/octokitWrapper.test.ts
@@ -5,6 +5,12 @@ jest.mock('./mocks/octokit/mockedOctokit', () => ({
   mockOctokitRequest: jest.fn()
 }))
 
+// To ensure that these tests work even inside of a github action
+// We force octokit to fail as it does outside of a github action
+jest.mock('@octokit/action', () => ({
+  Octokit: jest.fn().mockImplementation(() => { throw new Error('Not in github action') })
+}))
+
 describe('Octokit Wrapper class unmocked', () => {
   test('keeps octokit undefined in test mode', () => {
     const octokit = new OctokitWrapper()
