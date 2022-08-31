6f578fbc083c4d5d5bd6ee12487ae2d08508887b
River <riverliway@gmail.com>
2022-08-31 10:51:28 +0200

    feat(#14): create releases through the octokit API

--- src/API/octokitWrapper.ts
+++ src/API/octokitWrapper.ts
@@ -22,12 +22,14 @@ export class OctokitWrapper {
    * If the live github API is available, it sends the request
    * If not, it provides mocked data instead
    * @param request - The request to serve
+   * @param data - a package of data to send with the request
+   * The format of the data expected depends on the request endpoint
    * @returns live or mocked data
    */
-  async request (request: string): Promise<OctokitResponse<any>> {
+  async request (request: string, data?: any): Promise<OctokitResponse<any>> {
     if (this.octokit === undefined) {
       return mockOctokitRequest(request)
     }
-    return await this.octokit.request(request)
+    return await this.octokit.request(request, data)
   }
 }
