479874f844a09aa726da1bb703cdc30428b7e2e0
River <riverliway@gmail.com>
2022-08-31 23:42:28 +0200

    refact(#57): update branch data to grab from octokit API

--- src/changelog/variables/branch.ts
+++ src/changelog/variables/branch.ts
@@ -1,17 +1,36 @@
+import { OctokitWrapper } from '../../API/octokitWrapper'
 import { getGitRemoteURL } from '../../utils/gitRemoteOrigin'
+import { getRepo } from '../utils/getGithubInfo'
+
+const octokit = new OctokitWrapper()
 
 /**
  * Fetches the branch name and PR number from the CLI arguments
  * @return a markdown formatted version of the branch
  */
-export function evaluateBranch (): string {
+export async function evaluateBranch (): Promise<string> {
   const number = process.argv[3]
-  const name = process.argv[4]
 
+  const { headRef, baseRef } = await getBranchNames(number)
   const mainURL = getGitRemoteURL()
   const prURL = getPrURL(number)
 
-  return `#${number}   [main](${mainURL}) &larr; [${name}](${prURL})`
+  const gv = '`'
+  return `[${baseRef}](${mainURL}) &larr; [${gv}#${number}${gv} ${headRef}](${prURL})`
+}
+
+/**
+ * Gets the names of the branches involved in a PR
+ * @param id - the id of the PR to lookup
+ * @returns the name of the head and base branches
+ */
+async function getBranchNames (id: string): Promise<{ headRef: string, baseRef: string }> {
+  const request = `GET /repos/${getRepo()}/pulls/${id}`
+  const pullInfo = await octokit.request(request)
+  return {
+    headRef: pullInfo.data?.head.ref,
+    baseRef: pullInfo.data?.base.ref
+  }
 }
 
 /**
