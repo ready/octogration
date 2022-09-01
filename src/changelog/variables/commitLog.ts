3a43fdc2f23330c00ca6d8b32b556615d3710421
River <riverliway@gmail.com>
2022-09-01 10:46:27 +0200

    feat(#55): sort changelog by package JSON config sections

--- src/changelog/variables/commitLog.ts
+++ src/changelog/variables/commitLog.ts
@@ -1,32 +1,53 @@
 import { getGitRemoteURL } from '../../utils/gitRemoteOrigin'
-import { Commit, getCommitsByType } from '../parsers/commitParser'
+import { getPackageJson } from '../../utils/packageJson'
+import { Commit, getAllCommits, getCommitsByType, RawCommit } from '../parsers/commitParser'
 import { getGithubUserInfo, GithubUserInfo } from '../utils/getGithubInfo'
 
-// Which commit types belong under which section header
-const commitLogGrouping = new Map<string, string[]>()
-commitLogGrouping.set('Features', ['feat', 'perf'])
-commitLogGrouping.set('Bug Fixes', ['fix'])
-commitLogGrouping.set('Cleaning', ['refact', 'docs', 'style'])
-commitLogGrouping.set('Tests', ['test'])
-commitLogGrouping.set('Meta', ['build', 'ci'])
-
 /**
+ * Checks the package JSON config to find the section headers and type sorting
+ * If the `commitSections` field is a boolean, don't do any filtering
  * @returns all of the commits organized into sections based on type
  * and formatted into markdown for use in the release
  */
 export async function evaluateCommitLog (): Promise<string> {
-  const headers = [...commitLogGrouping.keys()]
-  const sections = await Promise.all(headers.map(async header => {
-    const types = commitLogGrouping.get(header) as string[]
+  const commitLogGrouping = getPackageJson().config.commitSections
+  const evalAll = typeof commitLogGrouping === 'boolean'
+  const commitLog = await (evalAll ? evaluateAllCommits() : evaluateAllSections(commitLogGrouping))
 
-    return await evaluateCommitSection(header, types)
-  }))
-
-  const commitLog = sections.reduce((log, sec) => `${log}${sec}`, '')
   if (commitLog === '') {
     console.log('No significant changes have been added to this release')
   }
   return commitLog
+}
+
+/**
+ * Grabs all commits regardless of type and formats them in HTML
+ * @returns all changelog lines in HTML
+ */
+async function evaluateAllCommits (): Promise<string> {
+  const commits = getAllCommits()
+  const htmlBlocks = await Promise.all(commits.map(async c => {
+    const githubUserInfo = await getGithubUserInfo(c.email)
+    return htmlifyRawCommit(githubUserInfo, c)
+  }))
+
+  return htmlBlocks.reduce((log, current) => `${log}${current}\n`, '')
+}
+
+/**
+ * Uses the commit sections to sort the commits by section
+ * @param commitSections - a map from headers to types included
+ * @returns all changelog sections in HTML/markdown
+ */
+async function evaluateAllSections (commitSections: { [type: string]: string[] }): Promise<string> {
+  const headers = Object.keys(commitSections)
+  const sections = await Promise.all(headers.map(async header => {
+    const types = commitSections[header]
+
+    return await evaluateCommitSection(header, types)
+  }))
+
+  return sections.reduce((log, sec) => `${log}${sec}`, '')
 }
 
 /**
@@ -42,7 +63,7 @@ async function evaluateCommitSection (header: string, types: string[]): Promise<
   if (commits.length === 0) return ''
 
   const sectionHeader = '# ' + header + '\n\n'
-  const changes = await Promise.all(commits.map(async (c) => {
+  const changes = await Promise.all(commits.map(async c => {
     const githubUserInfo = await getGithubUserInfo(c.email)
     return htmlifyCommit(githubUserInfo, c)
   }))
@@ -67,3 +88,20 @@ const htmlifyCommit = (userInfo: GithubUserInfo, commit: Commit): string =>
     ${commit.subject}
   </span>
 </div>`
+
+/**
+ * Formats the line in the changelog into html for the markdown release
+ * @param userInfo - the GitHub user information
+ * @param commit - the raw commit info without type or scope
+ * @returns the html code to place into the release body
+ */
+const htmlifyRawCommit = (userInfo: GithubUserInfo, commit: RawCommit): string =>
+ `<div>
+   <a href="https://github.com/${userInfo.username}">
+     <img valign="middle" alt="${userInfo.username}" src="https://images.weserv.nl/?url=${userInfo.avatar}?v=4&w=20&h=20&fit=cover&mask=circle" />
+   </a>
+   <span>
+     <code><a href="${getGitRemoteURL()}/commit/${commit.hash}">${commit.hash.substring(0, 7)}</a></code>
+     ${commit.message}
+   </span>
+ </div>`
