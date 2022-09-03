41bee59731b4f77c86c3e3d06daa00ddb50e4478
River <riverliway@gmail.com>
2022-09-03 18:50:14 +0200

    test(#104): add tests for changelog file when input file is invalid

--- src/changelog/parsers/changelogFileParser.ts
+++ src/changelog/parsers/changelogFileParser.ts
@@ -1,4 +1,5 @@
 import { readFileSync, writeFileSync } from 'fs'
+import { getPackageJson } from '../../utils/packageJson'
 
 interface ChangelogPost {
   title: string
@@ -18,14 +19,13 @@ export function appendToChangelogFile (post: ChangelogPost): void {
   writeChangelogFile(newPosts)
 }
 
-const CHANGELOG_FILE = 'automatic_changelog.json'
-
 /**
  * @returns an array of changelog posts read from the file
  */
 function readChangelogFile (): ChangelogPost[] {
+  const CHANGELOG_FILE = getPackageJson().config.changelogFileName
   try {
-    const file = readFileSync('')
+    const file = readFileSync(CHANGELOG_FILE)
     const posts = JSON.parse(file.toString())
     if (!(posts instanceof Array)) return []
     return posts
@@ -39,6 +39,7 @@ function readChangelogFile (): ChangelogPost[] {
  * @param posts - the array of posts to write
  */
 function writeChangelogFile (posts: ChangelogPost[]): void {
+  const CHANGELOG_FILE = getPackageJson().config.changelogFileName
   const file = JSON.stringify(posts)
   writeFileSync(CHANGELOG_FILE, file, { flag: 'wx' })
 }
