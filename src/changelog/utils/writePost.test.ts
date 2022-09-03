04dbc87bcf96cf7f25a750386d66969399635e54
River <riverliway@gmail.com>
2022-09-03 18:26:53 +0200

    test: add tests for write post

--- src/changelog/utils/writePost.test.ts
+++ src/changelog/utils/writePost.test.ts
@@ -4,16 +4,30 @@ import { appendToChangelogFile } from '../parsers/changelogFileParser'
 test('Writing the post yields the proper data', async () => {
   await writeChangelogPost()
   expect(mockedOutputPost).toEqual({
-    body: '',
+    body: 'Mocked body',
     env: 'Dev',
     timestamp: '2022-08-31T15:19:55.485Z',
-    title: '',
+    title: 'This is a mocked title',
     version: '1.2.3'
   })
 })
 
-test('Having writeChangelogToFile config option disabled prevents writing posts', () => {
+test('The pr title will not include markdown header', async () => {
+  mockedPrTitle = 'No markdown'
+  mockedPrBody = 'No markdown body'
+  await writeChangelogPost()
+  expect(mockedOutputPost).toEqual({
+    body: 'No markdown body',
+    env: 'Dev',
+    timestamp: '2022-08-31T15:19:55.485Z',
+    title: 'No markdown',
+    version: '1.2.3'
+  })
+})
+
+test('Having writeChangelogToFile config option disabled prevents writing posts', async () => {
   mockedWriteConfig = false
+  await writeChangelogPost()
   expect(appendToChangelogFile).not.toBeCalled()
 })
 
@@ -37,6 +51,16 @@ jest.mock('../../utils/packageJson', () => ({
       writeChangelogToFile: mockedWriteConfig
     }
   })
+}))
+
+let mockedPrTitle = '\n# This is a mocked title'
+jest.mock('../variables/prTitle', () => ({
+  evaluatePrTitle: () => mockedPrTitle
+}))
+
+let mockedPrBody = 'Mocked body'
+jest.mock('../variables/prBody', () => ({
+  evaluatePrBody: () => mockedPrBody
 }))
 
 afterEach(() => {
